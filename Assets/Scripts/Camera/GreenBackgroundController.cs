5a505eff4a837b8d85f72640e84bab1dbf0c8725
genta-ishizuka <genta.ishizuka@dena.com>
2025-03-07 01:37:09 +0900

    NDI実装とフォント改善

--- Assets/Scripts/Camera/GreenBackgroundController.cs
+++ Assets/Scripts/Camera/GreenBackgroundController.cs
@@ -3,22 +3,22 @@ using UnityEngine;
 public class GreenBackgroundController : MonoBehaviour 
 {
     private Camera mainCamera;
-    [SerializeField] private Canvas canvas; // 対象のCanvasを指定
+    // [SerializeField] private Canvas canvas; // 対象のCanvasを指定
 
-    void Start()
-    {
-        // Canvasのサイズを1920x1200に設定
-        RectTransform canvasRect = canvas.GetComponent<RectTransform>();
-        canvasRect.sizeDelta = new Vector2(1920, 1200);
+    // void Start()
+    // {
+    //     // Canvasのサイズを1920x1200に設定
+    //     RectTransform canvasRect = canvas.GetComponent<RectTransform>();
+    //     canvasRect.sizeDelta = new Vector2(1920, 1200);
 
-        // 実際のウィンドウサイズを1024x768に設定
-        Screen.SetResolution(1024, 768, false);
+    //     // 実際のウィンドウサイズを1024x768に設定
+    //     Screen.SetResolution(1024, 768, false);
 
-        // Canvasのスケールを調整して縮小表示
-        float scaleX = 1024f / 1920f;
-        float scaleY = 768f / 1200f;
-        canvas.transform.localScale = new Vector3(scaleX, scaleY, 1f);
-    }
+    //     // Canvasのスケールを調整して縮小表示
+    //     float scaleX = 1024f / 1920f;
+    //     float scaleY = 768f / 1200f;
+    //     canvas.transform.localScale = new Vector3(scaleX, scaleY, 1f);
+    // }
 
     void Awake()
     {
