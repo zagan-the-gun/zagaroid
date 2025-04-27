db993312d8551de5523f763e68c1b560d7019bcc
genta-ishizuka <genta.ishizuka@dena.com>
2025-04-27 19:24:59 +0900

    字幕AIからWebSockets経由で字幕を受け取る&OBSにWebSockets経由で字幕を送信する機能を実装

--- Assets/Scripts/ConfigLoader.cs
+++ Assets/Scripts/ConfigLoader.cs
@@ -2,26 +2,21 @@ using System.IO;
 using Newtonsoft.Json;
 using UnityEngine;
 
-public class ConfigLoader : MonoBehaviour
-{
+public class ConfigLoader : MonoBehaviour {
     private static ConfigLoader instance;
     private Config config;
 
     [System.Serializable]
-    public class Config
-    {
+    public class Config {
         public string DeepLApiClientKey; // APIキーを格納するプロパティ
+        public string ObsWebSocketsPassword;
     }
 
-    public static ConfigLoader Instance
-    {
-        get
-        {
-            if (instance == null)
-            {
+    public static ConfigLoader Instance {
+        get {
+            if (instance == null) {
                 instance = FindObjectOfType<ConfigLoader>();
-                if (instance == null)
-                {
+                if (instance == null) {
                     GameObject obj = new GameObject("ConfigLoader");
                     instance = obj.AddComponent<ConfigLoader>();
                     instance.LoadConfig(); // 初回アクセス時に設定を読み込む
@@ -31,25 +26,24 @@ public class ConfigLoader : MonoBehaviour
         }
     }
 
-    public void LoadConfig()
-    {
+    public void LoadConfig() {
         Debug.Log("Loading config...");
         string path = Path.Combine(Application.streamingAssetsPath, "config.json");
 
-        if (File.Exists(path))
-        {
+        if (File.Exists(path)) {
             string json = File.ReadAllText(path);
             config = JsonConvert.DeserializeObject<Config>(json);
             Debug.Log("Config loaded: " + json);
-        }
-        else
-        {
+        } else {
             Debug.LogError("Config file not found: " + path);
         }
     }
 
-    public string GetDeepLApiClientKey()
-    {
+    public string GetDeepLApiClientKey() {
         return config?.DeepLApiClientKey; // APIキーを返す
+    }
+
+    public string GetObsWebSocketsPassword() {
+        return config?.ObsWebSocketsPassword; // APIキーを返す
     }
 }
